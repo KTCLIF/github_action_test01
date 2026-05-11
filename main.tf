80eab03ed637cb864147dbdb467b049fcc52fa66
ljy <ktcloudljy@gmail.com>
2026-05-11 12:15:16 +0900

    destroy.yml 파일 추가함

--- main.tf
+++ main.tf
@@ -1,29 +1,25 @@
-# version 명시하기
 terraform {
   required_version = ">= 1.14.0"
+
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
     }
   }
-}
 
-# terraform 상태관리를 위한 remote 백엔드 설정
-backend "s3" {
-  bucket = "tfstate-bucket-40c65663"
-
-  key    = "test01/terraform.tfstate" # /test01/하위에 만들어 지도록
-  region = "ap-northeast-2"
-
-  dynamodb_table = "terraform-lock-test01" # 미리 준비된 dynamodb 테이블의 이름을 명시하면 lock 상태가 자동으로 관리된다.
-
-  encrypt = true # tfstate 에는 민감한 정보가 있을수 있기 때문에 암호화
-}
-
-# 1. provider 설정
-provider "aws" {
-    region = "ap-northeast-2" # 서울 리전
+  backend "s3" {
+    bucket         = "tfstate-bucket-40c65663"
+    key            = "test01/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-lock-test01"
+    encrypt        = true
+  }
 }
 
 
